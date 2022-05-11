d7ada1cbf5a0eed1be58846b422ca070b3c228b8
Yuvaraj1701 <harshithsharsha99@gmail.com>
2022-05-11 14:00:57 +0530

    First Commit

---
+++ Day1_GitHub/src/com/indiabix/git/Day1.java
@@ -0,0 +1,9 @@
+package com.indiabix.git;
+
+public class Day1 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome To C2TC");
+		
+	}
+}
