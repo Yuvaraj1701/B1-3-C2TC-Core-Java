aa280406a32948b86f567293ed79436c46727a45
harshiths99 <harshithsharsha99@gmail.com>
2022-05-11 14:28:23 +0530

    First commit

---
+++ Day4_Packages/src/com/indiabix/pack1/Example1.java
@@ -0,0 +1,12 @@
+package com.indiabix.pack1;
+
+public class Example1 {
+	public void display()
+	{
+		System.out.println("Welcome to B1-3 - JFS Batch");
+	}
+	public static void main(String[] args) {
+		
+
+	}
+}
---
+++ Day4_Packages/src/com/indiabix/pack2/Example2.java
@@ -0,0 +1,10 @@
+package com.indiabix.pack2;
+
+import com.indiabix.pack1.*;
+
+public class Example2 {
+	public static void main(String[] args) {
+		Example1 e=new Example1();
+		e.display();
+	}
+}
