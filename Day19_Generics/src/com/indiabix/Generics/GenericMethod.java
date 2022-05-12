6817a66fa3c61f97ffa0c4944f0eded47b0c1c13
harshiths99 <harshithsharsha99@gmail.com>
2022-05-12 16:36:45 +0530

    First Commit

---
+++ Day19_Generics/src/com/indiabix/Generics/GenericMethod.java
@@ -0,0 +1,25 @@
+package com.indiabix.Generics;
+
+
+public class GenericMethod 
+{
+	public static <E>void display(E[] arr)
+	{ 
+		for (E itr:arr)
+		{
+			System.out.println(itr);
+			
+		}
+		System.out.println();
+	}
+	
+
+	public static void main(String[] args) {
+		Integer[] intArray= {10,20,30,40,50};
+		Character[] c= {'a', 'b','c', 'd','e', 'f'};
+		display(intArray);
+		display(c);
+		
+	}
+
+}
