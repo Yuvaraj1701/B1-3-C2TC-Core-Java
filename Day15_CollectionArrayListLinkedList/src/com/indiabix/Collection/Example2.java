6b09fa42fa3c06145a086b2704b7f2858319ae73
harshiths99 <harshithsharsha99@gmail.com>
2022-05-12 15:13:30 +0530

    First Commit

---
+++ Day15_CollectionArrayListLinkedList/src/com/indiabix/Collection/Example2.java
@@ -0,0 +1,17 @@
+package com.indiabix.Collection;
+
+import java.util.ArrayList;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		ArrayList<Object> a=new ArrayList<>();
+		for(int i=10;i<=30;i++)
+		{
+			a.add(i);
+		}
+		System.out.println(a);
+
+	}
+
+}
