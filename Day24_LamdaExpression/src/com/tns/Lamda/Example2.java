09e000111bf80f85b97871089bc23390444142a3
harshiths99 <harshithsharsha99@gmail.com>
2022-05-24 15:31:37 +0530

    First Commit

---
+++ Day24_LamdaExpression/src/com/tns/Lamda/Example2.java
@@ -0,0 +1,20 @@
+package com.tns.Lamda;
+
+//Lambda Expression with single parameter
+interface C
+{
+	String display(String str);
+}
+public class Example2 {
+
+	public static void main(String[] args) {
+		
+		C object=(String str)->
+		{
+			
+			return str;
+		};
+		System.out.println(object.display("Lambda Expression with Single Parameter"));
+
+	}
+}
